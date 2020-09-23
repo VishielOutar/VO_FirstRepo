e0d85624a741bd094f1b300bea9acfe2e2e4487c
Vishiel Outar <outarvishiel@gmail.com>
2020-09-23 00:01:55 +0000

    This is the fourth git command

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("'I am taking CS407 because I want to learn about Linux");
+		System.out.println("My favorite food is rice");
 	}
 
 }
